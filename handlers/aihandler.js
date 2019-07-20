5bdb1727bb584613447dbc0840b544bc58072885
Ben <ben.beehler@gmail.com>
2019-07-20 11:53:39 -0400

    training alg

--- handlers/aihandler.js
+++ handlers/aihandler.js
@@ -2,7 +2,7 @@ const brain = require('brain.js')
 
 const config = {
   binaryThresh: 0.5,
-  hiddenLayers: [3],     // array of ints for the sizes of the hidden layers in the network
+  hiddenLayers: [10],     // array of ints for the sizes of the hidden layers in the network
   activation: 'sigmoid',  // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh'],
   leakyReluAlpha: 0.01   // supported for activation type 'leaky-relu'
 }
@@ -11,10 +11,13 @@ const config = {
 const net = new brain.NeuralNetwork(config)
 
 net.train([
-	{input: [3, 3], output: [0]},
-  {input: [0, 1], output: [1]},
-  {input: [1, 0], output: [1]},
-  {input: [1, 1], output: [0]}])
+	{input: [3, 3, 3, 3, 3, 3, 3, 3, 3], output: [1]},
+  {input: [3, 3, 2, 3, 1, 2, 1, 1, 3], output: [1]},
+  {input: [0, 0, 0, 0, 0, 0, 0, 0, 0], output: [0]},
+  {input: [0, 0, 1, 1, 0, 0, 1, 0, 0], output: [0]}])
+`                                                                       `                                                                                                                                                                                                                                                                                                                                                                                   
+module.exports.evaluate = (query, callback) => {
+  let result = net.run(query)
 
-const output = net.run([1, 0])  // [0.987]
-console.log(output)
+  callback(result)
+}
