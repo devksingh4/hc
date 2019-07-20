c1184261c54730b280a9ab97110c9f665d8a6b32
BenBeehler <ben.beehler@gmail.com>
2019-07-20 17:40:32 -0400

    yes mc yes

--- handlers/aihandler.js
+++ handlers/aihandler.js
@@ -4,7 +4,7 @@ const request = require('request')
 
 const config = {
   binaryThresh: 0.5,
-  hiddenLayers: [10],     // array of ints for the sizes of the hidden layers in the network
+  hiddenLayers: [1],     // array of ints for the sizes of the hidden layers in the network
   activation: 'sigmoid',  // supported activation types: ['sigmoid', 'relu', 'leaky-relu', 'tanh'],
   leakyReluAlpha: 0.01   // supported for activation type 'leaky-relu'
 }
