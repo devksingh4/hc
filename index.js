3024140305540caaec7ac404fa7373a0b0cc70c9
Dev <devksingh4@gmail.com>
2019-07-20 16:56:44 -0400

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8000 || process.env.PORT
+const port = process.env.PORT || 8000
 const path = require('path')
 const passport = require('passport')
 const cookieParser = require('cookie-parser')
